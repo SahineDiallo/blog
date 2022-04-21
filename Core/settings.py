78064f4d4ec3872ae035255385bc98ae08228e57
Papis <psdiallo300@gmail.com>
2022-04-21 15:15:22 +0000

    collected static

--- Core/settings.py
+++ Core/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = str(os.environ.get("SECRET_KEY"))
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['sahishblog.pythonanywhere.com']
 
 
 # Application definition
@@ -157,7 +157,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = "/static/"
-STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
 STATIC_ROOT = os.path.join(BASE_DIR, "static_root")
 MEDIA_URL = "/media/"
 MEDIA_ROOT = os.path.join(BASE_DIR, "media_root")
